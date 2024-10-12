12522da643108c9243400988b94ca20bb7cba220
Akash2005sharma <akashsharma.cpp@gmail.com>
2024-10-12 13:42:00 +0530

    mongo

--- shopeasy/lib/db.js
+++ shopeasy/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const connect= async ()=>{
     if(mongoose.connections[0].readyState) return;
     try {
-        await mongoose.connect(process.env.MONGODB_URL,{
+        await mongoose.connect(process.env.MONGODB_URI,{
             // serverSelectionTimeoutMS: 500000  //increasing acsses time
         });
         
